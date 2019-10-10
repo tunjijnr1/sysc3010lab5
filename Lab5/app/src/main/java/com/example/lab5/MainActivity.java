1867b7dd172677740c8423a7b8eebd961fd3eda4
oluwafunbiaboderin <funbiaboderin@yahoo.com>
2019-10-10 17:52:26 -0400

    Added a new activity which was to display a message

--- Lab5/app/src/main/java/com/example/lab5/MainActivity.java
+++ Lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -11,9 +11,14 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
